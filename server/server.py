9d70c4109a8f3f6ad5aff263ccc7aa4474210075
aviiii <avi.j@somaiya.edu>
2021-02-03 13:11:48 +0530

    refining

--- server/server.py
+++ server/server.py
@@ -1,16 +1,21 @@
-from flask import Flask,render_template,request,flash
+from flask import Flask, render_template, request, flash
 from flask_cors import CORS, cross_origin
 from newsapi import NewsApiClient
-import requests,random,string
+import requests
+import random
+import string
 app = Flask(__name__)
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 newsapi = NewsApiClient(api_key='6d00c7e55c784d84b0f24f28defb09a6')
-N=20
-randomkey=''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(N))
+N = 20
+randomkey = ''.join(random.SystemRandom().choice(
+    string.ascii_uppercase + string.digits) for _ in range(N))
 app.secret_key = randomkey
-all_articles = lambda x:(newsapi.get_everything(
+
+
+def all_articles(x): return (newsapi.get_everything(
     q=x,
     sources='bbc-news,the-verge',
     domains='bbc.co.uk,techcrunch.com',
@@ -20,38 +25,45 @@ all_articles = lambda x:(newsapi.get_everything(
     sort_by='relevancy',
     page=1
 ))
+
+
 @app.route('/<ticker>')
 def hello_world(ticker):
-    data = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+ticker+'&outputsize=compact&apikey=DLNKMATBLTI7J8SR')
+    data = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' +
+                        ticker+'&outputsize=compact&apikey=DLNKMATBLTI7J8SR')
     data = data.json()
-    response=[]
+    response = []
     for something in data:
-        if something=='Meta Data':
+        if something == 'Meta Data':
             continue
-        data=data[something]
+        data = data[something]
     for a in data:
-        temp={'timestamp':a}
+        temp = {'timestamp': a}
         for x in data[a]:
-            temp[x[3:]]=data[a][x]
+            temp[x[3:]] = data[a][x]
         response.append(temp)
     print('complete')
-    return {'series':response}
+    return {'series': response}
+
 
 @app.route('/news/<search_term>')
 def search(search_term):
-    temp=all_articles(search_term)['articles']
+    temp = all_articles(search_term)['articles']
     if not temp:
-        return {'all_articles':all_articles('finance')['articles']}
-    return {'all_articles':temp}
+        return {'all_articles': all_articles('finance')['articles']}
+    return {'all_articles': temp}
+
 
 @app.route('/graph')
 def render_graph_page():
     return render_template('graph.html')
 
+
 @app.route('/news')
 def render_news_page():
     return render_template('news.html')
 
+
 if __name__ == '__main__':
-    app.debug=True
+    app.debug = True
     app.run()
